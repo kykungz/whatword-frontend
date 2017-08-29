2548a976d3575e7123d16b1809ff96cf014b49d2
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2017-08-29 21:35:27 +0700

    add api-fallback

--- dist/server.js
+++ dist/server.js
@@ -1,11 +1,14 @@
 var express = require('express')
+var history = require('connect-history-api-fallback')
 var path = require('path')
 var serveStatic = require('serve-static')
 var port = process.env.PORT || 8080
 
 app = express()
 
+app.use(history())
 app.use(serveStatic(__dirname))
+
 app.listen(port, () => {
   console.log('server started ' + port)
 })
