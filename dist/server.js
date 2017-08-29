754624d8da58f0855900a4012ed2d2d457c326bb
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2017-08-29 21:50:10 +0700

    use new syntax

--- dist/server.js
+++ dist/server.js
@@ -1,8 +1,7 @@
-var express = require('express')
-var history = require('connect-history-api-fallback')
-var path = require('path')
-var serveStatic = require('serve-static')
-var port = process.env.PORT || 8080
+const express = require('express')
+const history = require('connect-history-api-fallback')
+const serveStatic = require('serve-static')
+const port = process.env.PORT || 8080
 
 app = express()
 
