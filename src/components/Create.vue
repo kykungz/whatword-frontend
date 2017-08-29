98e280fa5fc04426971b1fb5093794b54fc554b8
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2017-08-30 00:14:27 +0700

    Add build files

--- src/components/Create.vue
+++ src/components/Create.vue
@@ -1,7 +1,7 @@
 <template lang="html">
   <div id="create">
     <div class="container">
-      <h1 class="text-center">What Word is it ????CREATE??</h1>
+      <h1 class="text-center">What Word is it ?</h1>
       <hr>
       <div class="content">
         <div class="form-group">
@@ -27,7 +27,7 @@
         </div>
         <button :disabled="loading" v-on:click="submit()" class="btn btn-warning btn-lg btn-block">
           <icon v-show="loading" name="circle-o-notch" spin></icon>
-          Create
+          {{ this.loading ? 'Creating...' : 'Create' }}
         </button>
       </div>
     </div>
