455a13d01074fce7fd9f7faa22cc6ae175494993
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2017-12-23 03:46:03 +0700

    Deploy to gh-pages

--- src/libraries/variables.js
+++ src/libraries/variables.js
@@ -1,3 +1,3 @@
 export const ORIGIN_API_URL = process.env.NODE_ENV === 'production' ? 'https://whatword-api.herokuapp.com' : 'http://192.168.2.100:5000'// 'http://localhost:5000'
 
-export const ORIGIN_URL = process.env.NODE_ENV === 'production' ? 'https://whatword.herokuapp.com' : 'http://localhost:8080'
+export const ORIGIN_URL = process.env.NODE_ENV === 'production' ? 'https://kykungz.github.io/whatword' : 'http://localhost:8080'
