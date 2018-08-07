00caca17e2de357d5604903a679919f2f8844f62
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2018-08-07 20:40:21 +0700

    :recycle: Refactor code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,38 +1,36 @@
 <template>
   <div class="container">
-    <div id="home">
-      <div class="text-left content form-group">
-        <label>Enter Game ID:</label>
-        <input v-model="roomId" :class="{'is-invalid': error}" class="mb-2 text-center form-control form-control-lg" placeholder="GAME ID">
-        <label v-show="error" class="text-danger">
-          {{ error }}
-        </label>
-        <div class="options">
-          <button :disabled="loading" @click="redirect('Play')" class="btn btn-primary btn-lg btn-block">
+    <div class="text-left content form-group">
+      <label>Enter Game ID:</label>
+      <input v-model="roomId" :class="{'is-invalid': error}" class="mb-2 text-center form-control form-control-lg" placeholder="GAME ID">
+      <label v-show="error" class="text-danger">
+        {{ error }}
+      </label>
+      <div class="options">
+        <button :disabled="loading" @click="redirect('play')" class="btn btn-primary btn-lg btn-block">
+          <icon v-if="loading" name="circle-notch" spin />
+          <icon v-else name="rocket" /> Play
+        </button>
+        <div class="d-flex admin">
+          <button :disabled="loading" @click="redirect('scoreboard')" class="w-50 btn btn-danger btn-lg">
             <icon v-if="loading" name="circle-notch" spin />
-            <icon v-else name="rocket" /> Play
+            <icon v-else name="star" /> Scoreboard
           </button>
-          <div class="d-flex admin">
-            <button :disabled="loading" @click="redirect('Scoreboard')" class="w-50 btn btn-danger btn-lg">
-              <icon v-if="loading" name="circle-notch" spin />
-              <icon v-else name="star" /> Scoreboard
-            </button>
-            <button :disabled="loading" @click="redirect('Dashboard')" class="w-50 btn btn-dark btn-lg">
-              <icon v-if="loading" name="circle-notch" spin />
-              <icon v-else name="sliders-h" /> Dashboard
-            </button>
-          </div>
+          <button :disabled="loading" @click="redirect('dashboard')" class="w-50 btn btn-dark btn-lg">
+            <icon v-if="loading" name="circle-notch" spin />
+            <icon v-else name="sliders-h" /> Dashboard
+          </button>
         </div>
       </div>
-      <hr>
-      <h3 class="text-center">OR</h3>
-      <hr>
-      <div class="content">
-        <router-link :to="{ name: 'create' }" :disabled="loading" class="btn btn-warning btn-lg btn-block">
-          <icon v-if="loading" name="circle-notch" spin />
-          <icon v-else name="pencil-alt" /> Create New Game
-        </router-link>
-      </div>
+    </div>
+    <hr>
+    <h3 class="text-center">OR</h3>
+    <hr>
+    <div class="content">
+      <router-link :to="{ name: 'create' }" :disabled="loading" class="btn btn-warning btn-lg btn-block">
+        <icon v-if="loading" name="circle-notch" spin />
+        <icon v-else name="pencil-alt" /> Create New Game
+      </router-link>
     </div>
     <div class="mt-4 content d-flex justify-content-between align-items-center">
       <router-link :to="{ name: 'home' }">
@@ -73,13 +71,16 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-.options > * + * {
-  margin-top: 4px;
+<style lang="scss" scoped>
+.options {
+  > * + * {
+    margin-top: 4px;
+  }
 }
 
-.admin > .btn + .btn {
-  margin-left: 4px;
+.admin {
+  > .btn + .btn {
+    margin-left: 4px;
+  }
 }
 </style>
