a191447e0fa5d9fe4c1fc7c3450f2d88e5f08ad7
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2018-08-07 14:33:11 +0700

    :boom: Update to @vue/cli 3.0

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,29 @@
 <template>
   <div id="app">
-    <transition name="fade" mode="out-in" appear>
-      <div v-if="shouldShow()">
-        <github-corner />
-        <div class="header container">
-          <h1 class="text-center">What Word is it ?</h1>
-          <hr>
-        </div>
-      </div>
-    </transition>
-    <transition name="fade" mode="out-in" appear>
-      <router-view />
-    </transition>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import GithubCorner from '@/components/GithubCorner'
-
-export default {
-  name: 'app',
-  components: { GithubCorner },
-  methods: {
-    shouldShow () {
-      return ['Home', 'Dashboard', 'Create'].includes(this.$route.name)
-    }
-  }
-}
-</script>
-
-<style>
+<style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  text-align: center;
   color: #2c3e50;
 }
-
-.header {
-  text-align: center;
-  margin-top: 60px;
-}
-
-.content {
-  max-width: 500px;
-  margin: auto;
-}
-.fade-enter-active, .fade-leave-active {
-  transition: opacity 0.3s;
-}
-
-.fade-enter, .fade-leave-active {
-  opacity: 0;
-}
-
-.fullwidth {
-  width: 100%;
-}
-
-.password {
-  -webkit-text-security: disc;
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
--- src/libraries/customAxios.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-import config from '@/config'
-
-const customAxios = axios.create({
-  baseURL: config.ORIGIN_API_URL
-})
-
-export default customAxios
