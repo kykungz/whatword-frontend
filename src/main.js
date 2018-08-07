040144e19d481fb5c653b254071fb6ef2298226d
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2018-08-07 18:31:17 +0700

    :lipstick: Add css

--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import Icon from 'vue-awesome/components/Icon'
+import 'vue-awesome/icons'
+
+Vue.component('icon', Icon)
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/store.js
+++ src/store.js
@@ -4,7 +4,20 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    admins: [],
+  },
+  getters: {
+    admins: state => state.admins,
+  },
+  mutations: {
+    PUSH_ADMIN(state, { id, password }) {
+      state.admins.push({ id, password })
+    },
+  },
+  actions: {
+    pushAdmin(context, { id, password }) {
+      context.commit('PUSH_ADMIN', { id, password })
+    },
+  },
 })
