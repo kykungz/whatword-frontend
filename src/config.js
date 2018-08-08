f629ea35b91de5702209616ce246e6f5fb20353d
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2018-08-09 01:04:45 +0700

    :wrench: Change config to local

--- src/config.js
+++ src/config.js
@@ -4,9 +4,8 @@ const config = {
     ORIGIN_URL: 'https://kykungz.github.io/whatword/#',
   },
   development: {
-    ORIGIN_API_URL: 'http://192.168.1.104:5000',
-    // ORIGIN_URL: 'http://localhost:8080/#',
-    ORIGIN_URL: window.location.origin + '/#',
+    ORIGIN_API_URL: 'http://localhost:5000',
+    ORIGIN_URL: 'http://localhost:8080/#',
   },
   // development: {
   //   ORIGIN_API_URL: 'https://whatword-api.herokuapp.com',
