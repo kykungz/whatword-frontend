ccc588fb1bdb894ca393fd0d3f816f3519686dae
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2019-08-08 21:20:56 +0700

    Updates

---
+++ precache-manifest.9c013895e62a4ba7c3e7009320094f4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "5d849210e8ee745456dd",
+    "url": "js/chunk-vendors.5d849210.js"
+  },
+  {
+    "revision": "5b51d37e1e3379bbf0f4",
+    "url": "js/app.5b51d37e.js"
+  },
+  {
+    "revision": "e8ebc8e068d2b96d11e2a4651eff5368",
+    "url": "index.html"
+  },
+  {
+    "revision": "5d849210e8ee745456dd",
+    "url": "chunk-vendors.92be0538.css"
+  },
+  {
+    "revision": "5b51d37e1e3379bbf0f4",
+    "url": "app.c69b65a5.css"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.6e193bc26143e266c8f52d3ec2ac8faf.js"
+  "precache-manifest.9c013895e62a4ba7c3e7009320094f4e.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "whatword"});
